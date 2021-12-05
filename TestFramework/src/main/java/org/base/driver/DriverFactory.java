ca1949e17df3232322d429e97e8188a2eee8b9eb
Nick <nkozhe@gmail.com>
2021-12-05 20:17:41 +0100

    Adding basic webdriver version

--- TestFramework/src/main/java/org/base/driver/DriverFactory.java
+++ TestFramework/src/main/java/org/base/driver/DriverFactory.java
@@ -12,7 +12,7 @@ public class DriverFactory {
         if(webDriverWrapper != null){
             return webDriverWrapper;
         }
-        switch (Configuration.getProperty("browser")){
+        switch (new Configuration().getProperty("browser")){
             case "Chrome":
                 webDriverWrapper = new WebDriverWrapper(new ChromeDriver());
                 break;
