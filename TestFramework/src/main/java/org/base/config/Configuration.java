7e19f12cfc58e04f470e2dae3a49453020ab8b2f
Nick <nkozhe@gmail.com>
2021-12-05 20:18:27 +0100

    Adding basic webdriver version

--- TestFramework/src/main/java/org/base/config/Configuration.java
+++ TestFramework/src/main/java/org/base/config/Configuration.java
@@ -1,21 +1,17 @@
 package org.base.config;
 
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class Configuration {
-    private static Properties properties;
+    private static Properties properties = new Properties();
     private static final String configName = "config.properties";
 
-    private Configuration(){
-        properties = new Properties();
-    }
-
-    public static String getProperty(String propertyName){
+    public String getProperty(String propertyName){
         try {
-            FileReader reader = new FileReader(configName);
+            InputStream reader = getClass().getClassLoader().getResourceAsStream(configName);
             properties.load(reader);
             reader.close();
             return properties.getProperty(propertyName);
