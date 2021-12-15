cea631d7848b2b1eb3afda81e358325b6a4a3fed
Iryna <iryna.kuzmina.mail@gmail.com>
2021-12-15 17:38:33 +0100

    Dependency injection applied to the AddToCartTest

--- TestFramework/src/test/java/org/testframework/coolblue/AddToCartTest.java
+++ TestFramework/src/test/java/org/testframework/coolblue/AddToCartTest.java
@@ -1,33 +1,44 @@
 package org.testframework.coolblue;
 
 import org.base.config.Configuration;
-import org.junit.Assert;
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.pages.*;
 import org.testframework.coolblue.common.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class AddToCartTest extends BaseTest {
 
-    @Test
+    private MainPage mainPage;
+    private CategoryLaptopDesktopMonitor categoryLaptopDesktopMonitor;
+    private CategoryLaptop categoryLaptop;
+    private ProductsListPage productsListPage;
+    private AlmostYoursPage almostYoursPage;
+
+    @Test(groups = "Smoke")
     public void addToCartTest(){
+        mainPage = new MainPage(driver);
+        categoryLaptopDesktopMonitor = new CategoryLaptopDesktopMonitor(driver);
+        categoryLaptop = new CategoryLaptop(driver);
+        productsListPage = new ProductsListPage(driver);
+        almostYoursPage = new AlmostYoursPage(driver);
+
         driver.get(Configuration.getProperty("url"));
         //Accept cookie
         WebElement acceptCookieButton = driver.findElement(By.name("accept_cookie"));
         acceptCookieButton.click();
         //Click on the Computer & Tablets link
-        pageStorage.mainPage.getComputersAndTabletsLink().click();
+        mainPage.getComputersAndTabletsLink().click();
         //Click on the Laptops, desktops & monitoren link
-        pageStorage.mainPage.getLaptopDesktopsMonitorsLink().click();
+        mainPage.getLaptopDesktopsMonitorsLink().click();
         //Click on the Laptops
-        pageStorage.laptopDesktopMonitorCategoryPage.getLaptopsFilter().click();
+        categoryLaptopDesktopMonitor.getLaptopsFilter().click();
         //Click on the Alle laptops
-        pageStorage.laptopCategoryPage.getAllLaptopsFilter().click();
+        categoryLaptop.getAllLaptopsFilter().click();
         //Get the add to cart button on the first product (laptop) on the page
-        pageStorage.productsListPage.getAddButtonsList().get(0).click();
+        productsListPage.getAddButtonsList().get(0).click();
         //Assert the next page is Bijna Van Jou
-        Assert.assertTrue(pageStorage.almostYoursPage.getCartButton().isDisplayed());
-
-
+        Assert.assertTrue(almostYoursPage.getCartButton().isDisplayed());
     }
 }
